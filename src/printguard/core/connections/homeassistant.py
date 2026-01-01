991198b532c739807fda23be19d84febc2efbfe5
Oliver Bravery <oliverbravery@gmail.com>
2026-01-01 16:34:54 +0000

    Add methods to retrieve Home Assistant entity states and individual entity details, along with new data models for context and entity state management

--- src/printguard/core/connections/homeassistant.py
+++ src/printguard/core/connections/homeassistant.py
@@ -1,8 +1,12 @@
-from typing import Literal
+from typing import Literal, Optional, List, Dict, Any
+from datetime import datetime
+from httpx import RequestError
 from pydantic import BaseModel
 
 from ..db.base import BaseConfig
 from ..db.types import ConnectionType
+from ..db.schemas.tables.components import DeviceComponent
+from ..db.types import ComponentType
 from .base import BaseConnection
 from ..networking import http_client, RequestParams, HTTPMethod, ResponseData
 
@@ -25,6 +29,19 @@ class StatusComponentConfig(HABaseComponentConfig):
 class ControlComponentConfig(HABaseComponentConfig):
     entity_id: str
 
+class HAContext(BaseModel):
+    id: str
+    parent_id: Optional[str] = None
+    user_id: Optional[str] = None
+
+class HAEntityState(BaseModel):
+    entity_id: str
+    state: str
+    attributes: Dict[str, Any]
+    last_changed: datetime
+    last_updated: datetime
+    context: HAContext
+
 class HomeAssistantConnection(BaseConnection):
 
     _connection_config: HomeAssistantConnectionConfig
@@ -37,11 +54,41 @@ class HomeAssistantConnection(BaseConnection):
     async def is_healthy(self) -> bool:
         response: ResponseData = await http_client.run_async(
             params=RequestParams(
-                url=f"{self._connection_config.url}/api/health",
+                url=f"{self._connection_config.url}/api/",
                 method=HTTPMethod.GET,
                 headers=self.headers,
                 params=None,
                 json_data=None
             )
         )
-        return response.is_success
+        return response.content.get("message") == "API running." and response.is_success and response.status_code == 200
+
+    async def _get_entities(self) -> List[DeviceComponent]:
+        response: ResponseData = await http_client.run_async(
+            params=RequestParams(
+                url=f"{self._connection_config.url}/api/states",
+                method=HTTPMethod.GET,
+                headers=self.headers,
+                params=None,
+                json_data=None
+            )
+        )
+        if response.is_success and response.status_code == 200:
+            return [HAEntityState(**entity) for entity in response.content]
+        else:
+            raise RequestError(f"Failed to get entities. Status code: {response.status_code}, Content: {response.content}")
+
+    async def _get_entity(self, entity_id: str) -> HAEntityState:
+        response: ResponseData = await http_client.run_async(
+            params=RequestParams(
+                url=f"{self._connection_config.url}/api/states/{entity_id}",
+                method=HTTPMethod.GET,
+                headers=self.headers,
+                params=None,
+                json_data=None
+            )
+        )
+        if response.is_success and response.status_code == 200:
+            return HAEntityState(**response.content)
+        else:
+            raise RequestError(f"Failed to get entity. Status code: {response.status_code}, Content: {response.content}")
