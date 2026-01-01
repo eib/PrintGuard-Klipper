4348ea4660a9f21bf5de3ca04690d7bbaa1639bd
Oliver Bravery <oliverbravery@gmail.com>
2026-01-01 16:02:30 +0000

    Refactor BaseConnection methods for clarity and add get_status_states method for retrieving status entity states

--- src/printguard/core/connections/base.py
+++ src/printguard/core/connections/base.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 
 from ..db.schemas.tables.components import DeviceComponent
 
@@ -12,20 +12,20 @@ class BaseConnection:
         """Check if the connection is healthy."""
         raise NotImplementedError("The 'is_healthy' connection function has not been implemented.")
 
-    async def get_cameras(self, camera_ids: Optional[List[str]]) -> List[DeviceComponent]:
+    async def get_camera_entities(self, camera_ids: Optional[List[str]]) -> List[DeviceComponent]:
         """Get the cameras from the connection. 
         If camera_ids is provided, only return the cameras with the given ids.
         Returns a list of DeviceComponent objects with the connection's specific config.
         """
         raise NotImplementedError("The 'get_cameras' connection function has not been implemented.")
 
-    async def get_statuses(self, status_ids: Optional[List[str]]) -> List[DeviceComponent]:
+    async def get_status_entities(self, status_ids: Optional[List[str]]) -> List[DeviceComponent]:
         """Get the statuses from the connection. If status_ids is provided, only return the statuses with the given ids.
         Returns a list of DeviceComponent objects with the connection's specific config.
         """
         raise NotImplementedError("The 'get_statuses' connection function has not been implemented.")
 
-    async def get_controls(self, control_ids: Optional[List[str]]) -> List[DeviceComponent]:
+    async def get_control_entities(self, control_ids: Optional[List[str]]) -> List[DeviceComponent]:
         """Get the controls from the connection. If control_ids is provided, only return the controls with the given ids.
         Returns a list of DeviceComponent objects with the connection's specific config.
         """
@@ -36,3 +36,9 @@ class BaseConnection:
         Returns True if the control was triggered successfully, False otherwise.
         """
         raise NotImplementedError("The 'trigger_control' connection function has not been implemented.")
+
+    async def get_status_states(self, status_ids: Optional[List[str]]) -> List[str]:
+        """Get the latest state of the status entities. If status_ids is provided, only return the statuses with the given ids.
+        Returns a list of status states.
+        """
+        raise NotImplementedError("The 'get_status_states' connection function has not been implemented.")
